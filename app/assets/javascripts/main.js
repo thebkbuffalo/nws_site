18ce9f3a23aafd119b94c04cc554accd918462e1
Evan Berg <evan.m.berg@gmail.com>
2015-12-01 22:24:49 -0500

    cleaning up a bit

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
-  console.log("linked bitches");
   var url = window.location;
-        $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
-        $('ul.nav a').filter(function() {
-             return this.href == url;
-        }).parent().addClass('active');
+    $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+    $('ul.nav a').filter(function() {
+         return this.href == url;
+    }).parent().addClass('active');
 });
