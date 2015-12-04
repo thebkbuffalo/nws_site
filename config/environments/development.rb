c499756fe6a93b172ccd2cc371f20b900aad011e
Evan Berg <evan.m.berg@gmail.com>
2015-12-03 21:00:10 -0500

    still having weirdness

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -48,10 +48,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => "587",
-    :domain => "mydomain.net",
     :user_name => "evan.m.berg@gmail.com",
     :password => "greathambino",
     :authentication => "plain",
     :enable_starttls_auto => true
   }
+
 end
