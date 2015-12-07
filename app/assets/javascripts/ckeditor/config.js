b6fcbc5dde6c603a98b053dc80bf09af8d7fa870
Evan Berg <evan.m.berg@gmail.com>
2015-12-07 00:55:09 -0500

    removed a few things from editor toolbar

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -3,3 +3,8 @@ CKEDITOR.editorConfig = function(config) {
   config.width = 1150;
   return true;
 };
+
+
+$(document).ready(function() {
+  CKEDITOR.config.removePlugins = 'font,save,source,newpage,docdrops,preview,print,templates,document,find,replace,selectall,form,radio,createplaceholder,flash,table,tabletools,horizontalrule,smiley,specialchar,pagebreak,iframe,insertpre,creatediv, blockquote,numberedlist,bulletlist,form,checkbox,radio,textarea,textfield,select,anchor,link';
+});
